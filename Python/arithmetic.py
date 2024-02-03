b1255f584e193a76662d5244ec9048a50bb422ae
brigidsang2899@gmail.com <brigidsang2899@gmail.com>
2024-02-03 12:27:25 +0300

    git

---
+++ Python/arithmetic.py
@@ -0,0 +1,12 @@
+num1 =10;
+num2=5;
+sum=num1+num2;
+division=num1/num2;
+product=num1*num2;
+subtraction=num1-num2
+print("Sum of "+str(num1)+" and "+str(num2)+" is "+str(sum))
+print("Product of "+str(num1)+" and "+str(num2)+"i s "+str(product));
+print("Division of "+str(num1)+" and "+str(num2)+" is "+str(division));
+print("Subtraction of "+str(num1)+" and "+str(num2)+" is "+str(subtraction));
+print(type(sum));
+
